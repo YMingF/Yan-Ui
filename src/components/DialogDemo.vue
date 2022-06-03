32e3d8a833f5fdadf9bffc0b0c11684b2ac1bd2c
YMingF <834319313@qq.com>
2022-06-03 20:58:40 +0800

    [5-2: 创建Dialog组件]

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    Dialog示例
+    <Dialog/>
   </div>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue';
+
+export default {
+  components: {Dialog},
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <!--  遮罩层-->
+  <div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+      主要内容
+    </main>
+    <!--    按钮-->
+    <footer>
+      <Button>OK</Button>
+      <Button level="danger">Cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue';
+
+export default {
+  components: {Button},
+};
+</script>
+
+<style scoped>
+
+</style>
