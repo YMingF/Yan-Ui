7fec3033d836aa02578aa9ef6bb368714c717ea9
YMingF <834319313@qq.com>
2022-06-03 17:59:16 +0800

    [4-6: 加上不同theme下的button三个尺寸的样式]

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -12,6 +12,18 @@
     <Button>普普通</Button>
     <Button size="small">小小小</Button>
   </div>
+  <!--  link下的不同尺寸button-->
+  <div>
+    <Button theme="link" size="big">大大大</Button>
+    <Button theme="link">普普通</Button>
+    <Button size="small" theme="link">小小小</Button>
+  </div>
+  <!--  text下的不同尺寸button-->
+  <div>
+    <Button size="big" theme="text">大大大</Button>
+    <Button theme="text">普普通</Button>
+    <Button size="small" theme="text">小小小</Button>
+  </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue';
