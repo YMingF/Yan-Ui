58fca1b500f6f0d1fd11df342976772be8e18899
YMingF <834319313@qq.com>
2022-06-05 10:30:18 +0800

    [9-1: 修复部署时markdown动态加载不出来的问题]

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -5,19 +5,9 @@
 </template>
 
 <script lang='ts'>
-
-import {ref} from 'vue';
-
 export default {
   props: {
-    path: {type: String, required: true},
-  },
-  setup(props) {
-    const content = ref<string>('');
-    import(props.path).then(result => {
-      content.value = result.default;
-    });
-    return {content}
+    content: {type: String, required: true},
   },
 };
 </script>
--- src/router.ts
+++ src/router.ts
@@ -7,9 +7,12 @@ import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
 import {h} from 'vue';
 import Markdown from './components/Markdown.vue';
+import intro from './markdown/intro.md';
+import start from './markdown/start.md';
+import install from './markdown/install.md';
 
-const md = fileName => {
-  return h(Markdown, {path: `../markdown/${fileName}.md`, key: fileName});
+const md = string => {
+  return h(Markdown, {content: string, key: string});
 };
 const history = createWebHashHistory();
 export const router = createRouter({
@@ -21,9 +24,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         {path: '', redirect: '/doc/intro'}, //用''空字符来表示默认页面
-        {path: 'intro', component: md('intro')},
-        {path: 'start', component: md('start')},
-        {path: 'install', component: md('install')},
+        {path: 'intro', component: md(intro)},
+        {path: 'start', component: md(start)},
+        {path: 'install', component: md(install)},
         {path: 'switch', component: SwitchDemo},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
