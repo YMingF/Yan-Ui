9b7db2556c374613e62f5571f5fba49b99ecfba2
YMingF <834319313@qq.com>
2022-09-12 22:44:25 +0800

    Toast组件---完成支持HTML的功能展示

---
+++ src/components/Toast/EnableHtml.vue
@@ -0,0 +1,31 @@
+<demo>
+支持HTML
+</demo>
+
+<template>
+
+  <Button level="main" @click="showToast">点击弹出</Button>
+</template>
+
+<script lang='ts'>
+import Button from '../../lib/Button.vue';
+import {getCurrentInstance} from 'vue';
+
+export default {
+  components: {Button},
+  setup() {
+    const {$toast} = getCurrentInstance().appContext.config.globalProperties;
+
+    function showToast() {
+      $toast({
+        message: `<p style="color: red;">一探究竟</p>`,
+        enableHtml: true
+      });
+    }
+
+    return {
+      showToast
+    };
+  }
+};
+</script>
