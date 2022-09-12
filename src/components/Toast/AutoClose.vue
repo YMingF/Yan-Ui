25ae7742373029ceed40384ba9864bcf0a52ec0c
YMingF <834319313@qq.com>
2022-09-12 22:43:51 +0800

    Toast组件---完成自动关闭属性的展示

---
+++ src/components/Toast/AutoClose.vue
@@ -0,0 +1,31 @@
+<demo>
+设置弹窗是否自动关闭
+</demo>
+
+<template>
+  <Button level="main" @click="showToast">自动关闭</Button>
+  <Button level="main" @click="showToast({autoClose:false})">不自动关闭</Button>
+</template>
+
+<script lang='ts'>
+import Button from '../../lib/Button.vue';
+import {getCurrentInstance} from 'vue';
+
+export default {
+  components: {Button},
+  setup() {
+    const {$toast} = getCurrentInstance().appContext.config.globalProperties;
+
+    function showToast({autoClose}) {
+      $toast({
+        message: '提示信息',
+        autoClose
+      });
+    }
+
+    return {
+      showToast
+    };
+  }
+};
+</script>
---
+++ src/components/Toast/CloseDelay.vue
@@ -0,0 +1,30 @@
+<demo>
+设置自动关闭时长
+</demo>
+
+<template>
+  <Button level="main" @click="showToast">上方弹出</Button>
+</template>
+
+<script lang='ts'>
+import Button from '../../lib/Button.vue';
+import {getCurrentInstance} from 'vue';
+
+export default {
+  components: {Button},
+  setup() {
+    const {$toast} = getCurrentInstance().appContext.config.globalProperties;
+
+    function showToast() {
+      $toast({
+        message: '提示信息',
+        autoCloseDelay: 10
+      });
+    }
+
+    return {
+      showToast
+    };
+  }
+};
+</script>
