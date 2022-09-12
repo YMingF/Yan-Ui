cc72cce28fc185a54b960a9a774243d746f23930
YMingF <834319313@qq.com>
2022-09-12 22:44:42 +0800

    Toast组件---组件中Toast的view页面

--- src/views/Toast/ToastDemo.vue
+++ src/views/Toast/ToastDemo.vue
@@ -1,9 +1,32 @@
 <template>
-
+  <!--   控制展示位置-->
+  <Demo :component="ToastPosition"></Demo>
+  <!--  设计关闭按钮-->
+  <Demo :component="CloseButton"></Demo>
+  <!--  支持HTML-->
+  <Demo :component="EnableHtml"></Demo>
+  <!--  设置延时时长-->
+  <Demo :component="CloseDelay"></Demo>
+  <!--  设置是否自动关闭-->
+  <Demo :component="AutoClose"></Demo>
 </template>
 
 <script lang='ts'>
-export default {};
+import ToastPosition from '../../components/Toast/ToastPosition.vue';
+import CloseButton from '../../components/Toast/CloseButton.vue';
+import EnableHtml from '../../components/Toast/EnableHtml.vue';
+import CloseDelay from '../../components/Toast/CloseDelay.vue';
+import AutoClose from '../../components/Toast/AutoClose.vue';
+import Demo from '../../components/common/Demo.vue';
+
+export default {
+  components: {
+    Demo
+
+  }, setup() {
+    return {ToastPosition, CloseButton, EnableHtml, CloseDelay,AutoClose};
+  }
+};
 </script>
 
 <style lang='scss' scoped>
