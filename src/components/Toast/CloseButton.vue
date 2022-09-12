27eb3e201ac07269d1bf243a00bc6a935de8b10a
YMingF <834319313@qq.com>
2022-09-12 22:42:13 +0800

    Toast组件---完成自主设置关闭按钮内容的展示

---
+++ src/components/Toast/CloseButton.vue
@@ -0,0 +1,35 @@
+<demo>
+设置关闭按钮
+</demo>
+
+<template>
+  <Button level="main" @click="showToast">上方弹出</Button>
+</template>
+
+<script lang='ts'>
+import Button from '../../lib/Button.vue';
+import {getCurrentInstance} from 'vue';
+
+export default {
+  components: {Button},
+  setup() {
+    const {$toast} = getCurrentInstance().appContext.config.globalProperties;
+
+    function showToast() {
+      $toast({
+        message: '提示信息',
+        closeButton: {
+          text: '自己设置关闭按钮内容',
+          callback: () => {
+            console.log('有事启奏，无事退朝');
+          }
+        }
+      });
+    }
+
+    return {
+      showToast
+    };
+  }
+};
+</script>
