1571a18d38bdc783ec005d7e57097f643f1d04a8
YMingF <834319313@qq.com>
2022-09-25 10:16:13 +0800

    popover组件---加上API说明

--- src/views/Popover/PopoverDemo.vue
+++ src/views/Popover/PopoverDemo.vue
@@ -6,9 +6,15 @@
   <Demo :component="DiffTrigger" :desc-component="DiffTriggerDesc"></Demo>
   <!--  在浮窗内关闭-->
   <Demo :component="IsVisible" :desc-component="isVisibleDesc"></Demo>
+
+  <!--  API详情展示-->
+  <section id="api-container">
+    <h2 class="title_api">API</h2>
+    <api-table :head-title="titleArr" :body-data="tableContent"></api-table>
+  </section>
 </template>
 
-<script lang='ts'>
+<script lang='ts' setup>
 import Demo from '../../components/common/Demo.vue';
 import DiffPosition from '../../components/Popover/DiffPosition/diffPosition.vue';
 import DiffPosDesc from '../../components/Popover/DiffPosition/DiffPosDesc.vue';
@@ -16,13 +22,19 @@ import DiffTrigger from '../../components/Popover/DiffTrigger/DiffTrigger.vue';
 import DiffTriggerDesc from '../../components/Popover/DiffTrigger/DiffTriggerDesc.vue';
 import IsVisible from '../../components/Popover/isVisible/isVisible.vue';
 import isVisibleDesc from '../../components/Popover/isVisible/isVisibleDesc.vue';
+import ApiTable from '../../components/common/ApiTableContainer.vue';
 
-export default {
-  components: {Demo},
-  setup() {
-    return {DiffPosition, DiffPosDesc, IsVisible, isVisibleDesc, DiffTrigger, DiffTriggerDesc};
-  }
-};
+const titleArr = ['参数', '说明', '类型', '默认值'];
+const tableContent = [
+  ['content', `卡片内容`, 'slot', `<code>''</code>`
+  ],
+  [`position`,
+    `浮窗展示位置：<code>top</code><code>left</code><code>right</code><code>bottom</code>`,
+    `string`,
+    `<code>top</code>`],
+  [`trigger`, `触发浮窗显示的方式：<code>click:点击</code><code>hover:悬浮</code>`, `string`, `<code>click</code>`],
+  [`visible`, `调用组件时控制浮窗显示`, `boolean`, `<code>false</code>`]
+];
 </script>
 
 <style lang='scss' scoped>
@@ -52,6 +64,13 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+}
+
+// API部分
+#api-container {
+  .title_api {
+    margin: 38px 0 14px 0;
   }
 }
 </style>
